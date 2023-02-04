1db70ff7f3118adee3e9736f75f8e9e04c9bcb5c
christianbarillas <christianbarillasyt@gmail.com>
2023-02-04 17:29:40 -0600

    nuevos cambios

---
+++ const user = {.js
@@ -0,0 +1,15 @@
+const user = {
+    johnDoe: { 
+      age: 34,
+      email: 'johnDoe@freeCodeCamp.com'
+    }
+  };
+
+  const newobjet = 'emaefefeil';
+ const resultadoo = user.johnDoe = newobjet;
+  
+
+  
+
+  console.log(user)
+
